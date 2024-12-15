ffbf70ed9a500e829ab7f8f4796b4b8d8ad8457a
meiyi <meiyiguang0@outlook.com>
2024-12-15 19:29:29 +0800

    Fix server

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,21 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
 app.use('/api/status', status);
 app.use('/api/user', user);
 
+let frontend_dir = path.join(__dirname, '..', 'frontend', 'dist')
+
+app.use(express.static(frontend_dir));
+app.get('*', function (req, res) {
+    console.log("received request");
+    res.sendFile(path.join(frontend_dir, "index.html"));
+});
 
 const mongoEndpoint = "mongodb+srv://meiyi0guang:990104@cluster0.i2e12.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoEndpoint, { useNewUrlParser: true })
